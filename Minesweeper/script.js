95e6d4e3ce14c3bb49f22eee07e771c79cab9c78
caspardett@gmail.com <caspardett@gmail.com>
2022-04-07 15:13:18 +0200

    .

--- Minesweeper/script.js
+++ Minesweeper/script.js
@@ -9,7 +9,7 @@ for (let i = 0; i < 10; i++) {
     htmlToAdd += '<div class="row">';
     for (let x = 0; x < 10; x++) {
         const id = i * 10 + x;
-        htmlToAdd += `<div class="cell" data-row="${i}" data-cell="${x}"></div>`;
+        htmlToAdd += `<div class="cell hidden" data-row="${i}" data-cell="${x}"></div>`;
     }
     htmlToAdd += '</div>';
 }
@@ -25,7 +25,6 @@ for (let i = 0; i < field.length; i++) {
         field[i][j] = false;
     }
 }
-
 for (let i = 0; i < 10; i++) {
     getBomb()
 }
@@ -35,13 +34,20 @@ function getBomb() {
     let y = Math.floor(field.length * Math.random())
     if (field[x][y] !== undefined) {
         field[x][y] = undefined
-        const el = document.querySelector(`[data-row="${x}"][data-cell="${y}"]`)
-        const parent = document.createElement('div');
-        parent.innerHTML = '<img class="bomb" src="bomb.png">';
-        el.appendChild(parent.firstChild);
+        // const el = document.querySelector(`[data-row="${x}"][data-cell="${y}"]`)
+        // const parent = document.createElement('div');
+        // parent.innerHTML = '<img class="bomb" src="bomb.png">';
+        // el.appendChild(parent.firstChild);
     } else {
         getBomb()
     }
+}
+getSetBomb(0,0)
+
+function getSetBomb(x,y){
+    field[x][y] = undefined
+
+
 }
 
 console.log('field' + field)
@@ -58,12 +64,20 @@ console.log('field' + field)
 
 document.querySelectorAll(".cell")
     .forEach((e) => e.addEventListener("click", () => {
-        if (!field[e.getAttribute('data-row') - 1] [e.getAttribute('data-cell') - 1]) {
+
+    if(field[e.getAttribute('data-row')] [e.getAttribute('data-cell')] === undefined) {
+
+        console.log("Bombe getroffen")
+
+        showAllBombs()
+
+    }
+    else {if (!field[e.getAttribute('data-row')] [e.getAttribute('data-cell')]) {
 
             showField(e.getAttribute('data-row'), e.getAttribute('data-cell'))
 
 
-            field[e.getAttribute('data-row') - 1] [e.getAttribute('data-cell') - 1] = true
+            field[e.getAttribute('data-row')] [e.getAttribute('data-cell')] = true
 
 
             console.log(
@@ -74,139 +88,38 @@ document.querySelectorAll(".cell")
         }
 
 
-    }));
+    }}));
 
 
-function getBombCount(x, y) {
-    console.log(x, y)
-    x = Number(x)
-    y = Number(y)
-    let bombCounter = 0
+function showAllBombs(){
+    for(let x = 0; x > 10; x++){
+
+        for(let y = 0; y >10; y++){
+
+            if(field[x][y] === undefined){
+
+                const el = document.querySelector(`[data-row="${x}"][data-cell="${y}"]`)
+                const parent = document.createElement('div');
+                parent.innerHTML = '<img class="bomb" src="bomb.png">';
+                el.appendChild(parent.firstChild);
 
 
-    if (field[x] [y] !== true) {
-//LEFT
-
-        if (y !== 0) {
-            //Top Left
-            if (typeof (field[x - 1] [y - 1]) === 'undefined') {
-                bombCounter += 1
             }
 
-            console.log("left g")
-            //Middle Left
-            if (typeof (field[x] [y - 1]) === 'undefined') {
-                bombCounter += 1
-            }
 
-            console.log("midLeft g")
-            //Bottom Left
-            if (typeof (field[x + 1] [y - 1]) === 'undefined') {
-                bombCounter += 1
-            }
-            console.log("botLeft g")
-        }
-//RIGHT
-        if (y !== field.length) {
-            //Top Right
-            if (typeof (field[x - 1] [y + 1]) === 'undefined') {
-                bombCounter += 1
-            }
-
-            console.log("topRight g")
-            //Middle Right
-            if (typeof (field[x] [y + 1]) === 'undefined') {
-                bombCounter += 1
-            }
-            console.log("midRIght g")
-
-            //Bottom Right
-            if (typeof (field[x + 1] [y + 1]) === 'undefined') {
-                bombCounter += 1
-            }
-
-            console.log("botRight g")
-        }
-//TOP MIDDLE
-        if (x !== 0) {
-            //Top Middle
-            if (typeof (field[x - 1] [y]) === 'undefined') {
-                bombCounter += 1
-            }
-
-            console.log("top mid g")
         }
 
-//BOTTOM MIDDLE
-        if (x !== field.length) {
-
-            //Bottom Middle
-            if (typeof (field[x + 1] [y]) === 'undefined') {
-                bombCounter += 1
-            }
-            console.log("bot Mid g")
-        }
-        document.querySelector(`[data-row="${x}"][data-cell="${y}"]`).append(document.createTextNode(bombCounter))
 
     }
-    if (bombCounter === 0) {
 
-        nulls.append([x, y])
 
-        field[x + 1] [y] = true
-        field[x - 1] [y] = true
-        field[x + 1] [y + 1] = true
-        field[x - 1] [y - 1] = true
-        field[x] [y - 1] = true
-        field[x - 1] [y + 1] = true
-        field[x + 1] [y - 1] = true
-        field[x] [y + 1] = true
-
-        if (y !== 0) {
-            //Top Left
-            getBombCount(x - 1, y - 1)
-            //Middle Left
-            getBombCount(x, y - 1)
-            //Bottom Left
-            getBombCount(x + 1, y - 1)
-        }
-
-        if (x !== 0) {
-            //Top Middle
-            getBombCount(x - 1, y)
-        }
-
-        if (x !== field.length) {
-            //Bottom Middle
-            getBombCount(x + 1, y)
-        }
-
-        if (y !== field.length) {
-            //Top Right
-            getBombCount(x - 1, y + 1)
-            //Middle Right
-            getBombCount(x, y + 1)
-            //Bottom Right
-            getBombCount(x + 1, y + 1)
-        }
-    }
-
-    return bombCounter
 }
 
+
+
+
 function showField(x, y) {
-    if (getNeighboursBombCount(x, y) === 0) {
-        // getNeighboursBombCount(x - 1, y - 1)
-        // getNeighboursBombCount(x, y - 1)
-        // getNeighboursBombCount(x + 1, y - 1)
-        // getNeighboursBombCount(x - 1, y)
-        // getNeighboursBombCount(x + 1, y)
-        // getNeighboursBombCount(x - 1, y + 1)
-
-        // getNeighboursBombCount(x, y + 1)
-        // getNeighboursBombCount(x + 1, y + 1)
-    }
-
+    getNeighboursBombCount(x,y)
 }
 
 
@@ -214,7 +127,7 @@ function getNeighboursBombCount(x, y) {
     x = Number(x)
     y = Number(y)
     let bombCounter = 0
-    debugger
+
 
 
     if (y !== 0) {
@@ -241,7 +154,7 @@ function getNeighboursBombCount(x, y) {
         }
 
 
-        if (x !== field.length-1) {
+        if (x !== field.length - 1) {
             if (field[x + 1][y - 1] !== true) {
                 //Bottom Left
                 if (typeof (field[x + 1] [y - 1]) === 'undefined' && field[x + 1][y - 1] !== true) {
@@ -252,7 +165,7 @@ function getNeighboursBombCount(x, y) {
         }
     }
 //RIGHT
-    if (y !== field.length-1) {
+    if (y !== field.length - 1) {
 
 
         if (x !== 0) {
@@ -266,7 +179,7 @@ function getNeighboursBombCount(x, y) {
         }
 
 
-        if (y !== field.length-1) {
+        if (y !== field.length - 1) {
             if (field[x][y + 1] !== true) {
 
                 //Middle Right
@@ -278,7 +191,7 @@ function getNeighboursBombCount(x, y) {
         }
 
 
-        if (x !== field.length-1) {
+        if (x !== field.length - 1) {
             if (field[x + 1][y + 1] !== true) {
 
                 //Bottom Right
@@ -306,7 +219,7 @@ function getNeighboursBombCount(x, y) {
 
 
 //BOTTOM MIDDLE
-    if (x < field.length-1) {
+    if (x < field.length - 1) {
 
         if (field[x + 1][y] !== true) {
             //Bottom Middle
@@ -320,6 +233,7 @@ function getNeighboursBombCount(x, y) {
 
     if (field[x] [y] === false) {
         document.querySelector(`[data-row="${x}"][data-cell="${y}"]`).append(document.createTextNode(bombCounter))
+        document.querySelector(`[data-row="${x}"][data-cell="${y}"]`).classList.remove('hidden')
     }
 
     field[x][y] = true
@@ -335,32 +249,32 @@ function getNeighboursBombCount(x, y) {
                 getNeighboursBombCount(x - 1, y) //Top Mid
             }
         }
-        if (y !== field.length-1 && x !== 0) {
+        if (y !== field.length - 1 && x !== 0) {
             if (field[x - 1] [y + 1] !== true) {
                 getNeighboursBombCount(x - 1, y + 1)//Top Right
             }
         }
         if (y !== 0) {
-            if (field[x] [y - 1]) {
+            if (field[x] [y - 1] !== true) {
                 getNeighboursBombCount(x, y - 1)//Middle Left
             }
         }
-        if (y !== field.length-1) {
+        if (y !== field.length - 1) {
             if (field[x] [y + 1] !== true) {
                 getNeighboursBombCount(x, y + 1)//Middle Right
             }
         }
-        if (y !== 0 && x !== field.length) {
+        if (y !== 0 && x !== field.length - 1) {
             if (field[x + 1] [y - 1] !== true) {
                 getNeighboursBombCount(x + 1, y - 1)//Bottom Left}
             }
         }
-        if (x !== field.length-1) {
+        if (x !== field.length - 1) {
             if (field[x + 1] [y] !== true) {
                 getNeighboursBombCount(x + 1, y)//Bottom Mid
             }
         }
-        if (y !== field.length -1) {
+        if (y !== field.length - 1 && x !== field.length - 1) {
             if (field[x + 1] [y + 1] !== true) {
                 getNeighboursBombCount(x + 1, y + 1)//Bottom Right
             }
